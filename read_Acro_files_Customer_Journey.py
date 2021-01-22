3287f36ab4ee4f5d0dc804e62a02f0143109ca69
dritter-collab <71488163+dritter-collab@users.noreply.github.com>
2021-01-22 11:29:37 +0100

    Einlesen der Customer Journeys

--- read_Acro_files_Customer_Journey.py
+++ read_Acro_files_Customer_Journey.py
@@ -3,14 +3,17 @@ from avro.io import DatumReader
 from pathlib import Path
 import os
 import pandas as pd
+import json
+import avro.schema
+import copy
 
-fileDirProfiles = r"/Users/davidritter/Documents/userwerk/CDP_Recommendation/Avro-Files/Customer Profiles"
 fileDirJourneys = r"/Users/davidritter/Documents/userwerk/CDP_Recommendation/Avro-Files/Customer Journeys"
 fileExt = r".avro"
 df = pd.DataFrame()
 fileList = []
 counter = 0
 
+# Auslesen aller Avro-Daten in diesem Ordner
 for root, dirs, files in os.walk(fileDirJourneys):  # root = Path, dirs = folders, files = files
     for file in Path(str(root)).iterdir():
         string = str(file)
@@ -18,14 +21,22 @@ for root, dirs, files in os.walk(fileDirJourneys):  # root = Path, dirs = folder
             fileList.append(string)
 fileList = sorted(fileList)
 print(fileList)
+print(len(fileList))
+
+# reader = avro.datafile.DataFileReader(open('/Users/davidritter/Documents/userwerk/CDP_Recommendation/Avro-Files/Customer Journeys/ts=1590966000000/part-0-0.avro',"rb"),avro.io.DatumReader())
+# schema = reader.meta
+# print(schema)
 
 for files in fileList:
-    reader = DataFileReader(open(files, "rb"), DatumReader())
-    for user in reader:
-        df = df.append(user, ignore_index=True)
-        counter = counter + 1
-        print(counter)
-reader.close()
-
-df.to_csv('/Users/davidritter/Documents/userwerk/CDP_Recommendation/test.csv')
-df.to_json('/Users/davidritter/Documents/userwerk/CDP_Recommendation/test.json')
+    with open(files, 'rb') as f:
+        reader = DataFileReader(f, DatumReader())
+        metadata = copy.deepcopy(reader.meta)
+        schema_from_file = json.loads(metadata['avro.schema'])
+        users = [user for user in reader]
+        targetfile = "/Users/davidritter/Documents/userwerk/CDP_Recommendation/Avro-Files/transformed Avro-Files/Customer Journey/Journey_" + str(counter)
+        with open(targetfile, "w") as write_file:
+            users = str(users)
+            json.dump(users, write_file)
+            counter = counter + 1
+            print(counter)
+    reader.close()
